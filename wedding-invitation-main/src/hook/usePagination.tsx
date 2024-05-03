9758d091bb22f684b1c157b6510d2ef4fcb2fbb6
allopen08 <72002254+allopen08@users.noreply.github.com>
2024-05-03 21:15:08 +0900

    Delete wedding-invitation-main directory

--- wedding-invitation-main/src/hook/usePagination.tsx
+++
@@ -1,9 +0,0 @@
-import { useState } from "react";
-
-export const usePagination = () => {
-  const [page, setPage] = useState<number>(1);
-  const offset = (page - 1) * 5;
-  return{
-    page, setPage, offset
-  }
-}
--- wedding-invitation-main/styles/ImageLayout.tsx
+++
@@ -1,5 +0,0 @@
-import styled from "styled-components"
-
-export const AutoHeightImageWrapper = styled.section`
-
-`
